b4999ca96d5f36af9d1b31d2b26a1bf3767c5a7c
Allen Li <darkfeline@felesatra.moe>
2017-10-10 23:17:03 -0700

    Add org

---
+++ mir/dlsite/org.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2017 Allen Li
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Organizing works."""
+
+import logging
+import os
+from pathlib import Path
+from typing import NamedTuple
+
+from mir.dlsite import api
+from mir.dlsite import workinfo
+
+logger = logging.getLogger(__name__)
+
+
+def find_works(top_dir: 'PathLike') -> 'Iterable[Path]':
+    """Find DLsite works.
+
+    Yield Path instances to work directories, relative to top_dir.
+    """
+    for dirpath, dirnames, _filenames in os.walk(top_dir):
+        work_dirnames = [n for n in dirnames if workinfo.contains_rjcode(n)]
+        yield from (Path(dirpath, n).relative_to(top_dir) for n in work_dirnames)
+        for n in work_dirnames:
+            dirnames.remove(n)
+
+
+def calculate_path_renames(fetcher, works: 'Iterable[Path]') -> 'Iterable[PathRename]':
+    """Find rename operations to organize works.
+
+    Yield PathRename instances.
+    """
+    for path in works:
+        rjcode = workinfo.parse_rjcode(path.name)
+        work = fetcher(rjcode)
+        wanted_path = workinfo.work_path(work)
+        if path != wanted_path:
+            yield PathRename(path, wanted_path)
+        else:
+            logger.info('%s already correct', path)
+
+
+def remove_empty_dirs(top_dir: 'PathLike'):
+    for dirpath, dirnames, filenames in os.walk(top_dir, topdown=False):
+        if not os.listdir(dirpath):
+            os.rmdir(dirpath)
+
+
+class PathRename(NamedTuple):
+    """PathRename represents a rename operation."""
+    old: Path
+    new: Path
+
+    def execute(self, top_dir: Path):
+        old = top_dir / self.old
+        new = top_dir / self.new
+        new.parent.mkdir(parents=True, exist_ok=True)
+        logger.debug('Renaming %s to %s', old, new)
+        old.rename(new)
---
+++ tests/test_org.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2017 Allen Li
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from pathlib import Path
+
+import pytest
+
+from mir.dlsite import org
+from mir.dlsite import workinfo
+
+
+def test_find_works(tmpdir):
+    tmpdir.ensure('foo/RJ123/RJ456', dir=True)
+    got = list(org.find_works(str(tmpdir)))
+    assert got == [
+        Path('foo/RJ123'),
+    ]
+
+
+def test_calculate_path_renames(stub_fetcher):
+    works = [
+        Path('foo/RJ123'),
+        Path('group/series/RJ456 name'),
+    ]
+    got = list(org.calculate_path_renames(stub_fetcher, works))
+    assert got == [
+        org.PathRename(Path('foo/RJ123'), Path('group/series/RJ123 name')),
+    ]
+
+
+def test_remove_empty_dirs(tmpdir):
+    tmpdir.ensure('foo/bar/baz/sophie', dir=True)
+    tmpdir.ensure('foo/spam')
+
+    org.remove_empty_dirs(str(tmpdir))
+
+    assert os.listdir(str(tmpdir)) == ['foo']
+    assert os.listdir(str(tmpdir.join('foo'))) == ['spam']
+
+
+def test_PathRename_execute(tmpdir):
+    tmpdir.ensure('foo/bar/baz/sophie')
+    r = org.PathRename(Path('foo/bar/baz'),
+                       Path('baz'))
+    r.execute(Path(str(tmpdir)))
+    assert os.path.exists(os.path.join(str(tmpdir), 'baz/sophie'))
+
+
+@pytest.fixture
+def stub_fetcher():
+    def fetch(rjcode):
+        work = workinfo.Work(rjcode, 'name', 'group')
+        work.series = 'series'
+        return work
+    return fetch
